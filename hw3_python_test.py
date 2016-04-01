cc08affd8473a772bc5ee79589990bd195335932
Nolan Foster <nolanfoster@gmail.com>
2016-04-01 13:56:27 -0400

    fixed path

--- hw3_python_test.py
+++ hw3_python_test.py
@@ -2,23 +2,24 @@
 #!python
 import hw3 ,filecmp, os
 
+path = os.getenv('HOME') + '/Documents/CSE305/hw3/'
 output_path = os.getenv('HOME') + '/Documents/CSE305/hw3/test_cases/output'
 input_path = os.getenv('HOME') + '/Documents/CSE305/hw3/test_cases/input'
 
 print("HW 2 Tests:")
-for f in os.listdir(input_path+"/hw2"):
-    if os.path.isfile(os.path.join(input_path+"/hw2", f)):
-        hw3.hw3(input_path+"/hw2"+"/"+f,'output.txt')
-    if(filecmp.cmp(output_path+"/"+f.replace('input', 'output'), 'output.txt')):
+for f in os.listdir(path+"test_cases/input/hw2"):
+    if os.path.isfile(os.path.join(path+"test_cases/input/hw2", f)):
+        hw3.hw3(path+"test_cases/input/hw2"+"/"+f,'output.txt')
+    if(filecmp.cmp(path+"test_cases/output/"+f.replace('input', 'output'), 'output.txt')):
         print("            "+f+" : \033[1;32mPassed\033[0;0m")
     else:
         print("            "+f+" : \033[1;31mFailed\033[0;0m")
 os.remove('output.txt')
 print("HW 3 Tests:")
-for f in os.listdir(input_path+"/hw3"):
-    if os.path.isfile(os.path.join(input_path+"/hw3", f)):
-        hw3.hw3(input_path+"/hw3"+"/"+f,f.replace('input', 'output'))
-    if(filecmp.cmp(output_path+"/"+f.replace('input', 'output'), f.replace('input', 'output'))):
+for f in os.listdir(path+"test_cases/input/hw3"):
+    if os.path.isfile(os.path.join(path+"test_cases/input/hw3", f)):
+        hw3.hw3(path+"test_cases/input/hw3"+"/"+f,f.replace('input', 'output'))
+    if(filecmp.cmp(path+"test_cases/output/"+f.replace('input', 'output'), f.replace('input', 'output'))):
         print("            "+f+" : \033[1;32mPassed\033[0;0m")
         os.remove(f.replace('input', 'output'))
     else:
