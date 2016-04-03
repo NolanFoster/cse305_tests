c0692b44bdd2e0dcc9c8f48d4ae82bd738fe83ff
Nolan Foster <nolanfoster@gmail.com>
2016-04-03 09:51:38 -0400

    code clean up

--- hw3_test.py
+++ hw3_test.py
@@ -4,29 +4,22 @@ import hw3 ,filecmp, os, sys
 
 path = os.getenv('HOME') + '/Documents/CSE305/hw3/'
 
-print("HW 2 Tests:")
-for f in os.listdir(path+"test_cases/input/hw2"):
-    if os.path.isfile(os.path.join(path+"test_cases/input/hw2", f)):
-        try:
-            hw3.hw3(path+"test_cases/input/hw2"+"/"+f,f.replace('input', 'output'))
-        except:
-            print ("Unexpected error:", sys.exc_info())
+def python_test(hw):
+    for f in os.listdir(path+"test_cases/input/"+hw):
+        if os.path.isfile(os.path.join(path+"test_cases/input/"+hw, f)):
+            try:
+                hw3.hw3(path+"test_cases/input/"+hw+"/"+f,f.replace('input', 'output'))
+            except:
+                print ("Unexpected error:", sys.exc_info())
 
-    if(filecmp.cmp(path+"test_cases/output/"+f.replace('input', 'output'), f.replace('input', 'output'))):
-        print("            "+f+" : \033[1;32mPassed\033[0;0m")
-        os.remove(f.replace('input', 'output'))
-    else:
-        print("            "+f+" : \033[1;31mFailed\033[0;0m")
+        if(filecmp.cmp(path+"test_cases/output/"+f.replace('input', 'output'), f.replace('input', 'output'))):
+            print("            "+f+" : \033[1;32mPassed\033[0;0m")
+            os.remove(f.replace('input', 'output'))
+        else:
+            print("            "+f+" : \033[1;31mFailed\033[0;0m")
+
+print("HW 2 Tests:")
+python_test("hw2")
 
 print("HW 3 Tests:")
-for f in os.listdir(path+"test_cases/input/hw3"):
-    if os.path.isfile(os.path.join(path+"test_cases/input/hw3", f)):
-        try:
-            hw3.hw3(path+"test_cases/input/hw3"+"/"+f,f.replace('input', 'output'))
-        except:
-            print ("Unexpected error:", sys.exc_info())
-    if(filecmp.cmp(path+"test_cases/output/"+f.replace('input', 'output'), f.replace('input', 'output'))):
-        print("            "+f+" : \033[1;32mPassed\033[0;0m")
-        os.remove(f.replace('input', 'output'))
-    else:
-        print("            "+f+" : \033[1;31mFailed\033[0;0m")
+python_test("hw3")
