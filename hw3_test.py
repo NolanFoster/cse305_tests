b6c6b7e31d7100701236ae64ad9bb206a21030bf
Nolan Foster <nolanfoster@gmail.com>
2016-04-04 10:49:32 -0400

    Added more config option and partially workign sml

--- hw3_test.py
+++ hw3_test.py
@@ -4,9 +4,13 @@ import filecmp, os, sys
 path = os.getenv('HOME') + '/Documents/CSE305/hw3/'
 sys.path.insert(0, path+'/python')
 import hw3
-
+output = False
 def python_test(hw):
+    passed=0
+    total=0
+    failed = 0
     for f in os.listdir(path+"test_cases/input/"+hw):
+        total=total+1
         if os.path.isfile(os.path.join(path+"test_cases/input/"+hw, f)):
             try:
                 hw3.hw3(path+"test_cases/input/"+hw+"/"+f,'python_'+f.replace('input', 'output'))
@@ -14,14 +18,23 @@ def python_test(hw):
                 print ("Unexpected error:", sys.exc_info())
 
         if(filecmp.cmp(path+"test_cases/output/"+f.replace('input', 'output'), 'python_'+f.replace('input', 'output'))):
-            print("            "+f+" : \033[1;32mPassed\033[0;0m")
+            passed=passed+1
+            if output:
+                print("            "+f+" : \033[1;32mPassed\033[0;0m")
             os.remove('python_'+f.replace('input', 'output'))
         else:
-            print("            "+f+" : \033[1;31mFailed\033[0;0m")
+            failed=failed+1
+            if output:
+                print("            "+f+" : \033[1;31mFailed\033[0;0m")
+    print('\033[1;34m'+str(passed)+" tests passed of "+str(total)+'\033[0;0m')
 
 def java_test(hw):
+    passed=0
+    total=0
+    failed = 0
     os.system('javac tester.java')
     for f in os.listdir(path+"test_cases/input/"+hw):
+        total=total+1
         if os.path.isfile(os.path.join(path+"test_cases/input/"+hw, f)):
             try:
                 os.system('java tester ' + path+"test_cases/input/"+hw+"/"+f+' '+'java_'+f.replace('input', 'output'))
@@ -29,18 +42,41 @@ def java_test(hw):
                 print ("Unexpected error:", sys.exc_info())
 
         if(filecmp.cmp(path+"test_cases/output/"+f.replace('input', 'output'), 'java_'+f.replace('input', 'output'))):
-            print("            "+f+" : \033[1;32mPassed\033[0;0m")
+            passed=passed+1
+            if output:
+                print("            "+f+" : \033[1;32mPassed\033[0;0m")
             os.remove('java_'+f.replace('input', 'output'))
         else:
-            print("            "+f+" : \033[1;31mFailed\033[0;0m")
+            if output:
+                print("            "+f+" : \033[1;31mFailed\033[0;0m")
 
     os.remove('tester.class')
     os.remove('hw3.class')
+    print('\033[1;34m'+str(passed)+" tests passed of "+str(total)+'\033[0;0m')
 
-print("HW 2 Python Tests:")
+def sml_test(hw):
+    for f in os.listdir(path+"test_cases/input/"+hw):
+        if os.path.isfile(os.path.join(path+"test_cases/input/"+hw, f)):
+            try:
+                os.system('sml hw3.sml ' + path+"test_cases/input/"+hw+"/"+f+' '+'sml_'+f.replace('input', 'output'))
+            except:
+                print ("Unexpected error:", sys.exc_info())
+
+        if(filecmp.cmp(path+"test_cases/output/"+f.replace('input', 'output'), 'sml_'+f.replace('input', 'output'))):
+            print("            "+f+" : \033[1;32mPassed\033[0;0m")
+            os.remove('sml_'+f.replace('input', 'output'))
+        else:
+            print("            "+f+" : \033[1;31mFailed\033[0;0m")
+
+
+print('\033[1;37m'+"HW 2 Python Tests:"+'\033[0;0m')
 python_test("hw2")
-print("HW 2 Java Tests:")
+
+print('\033[1;37m'+"HW 2 Java Tests:"+'\033[0;0m')
 java_test("hw2")
 
-print("HW 3 Tests:")
+print('\033[1;37m'+"HW 3 Python Tests:"+'\033[0;0m')
 python_test("hw3")
+
+print('\033[1;37m'+"HW 3 Java Tests:"+'\033[0;0m')
+java_test("hw3")
--- tester.java
+++ tester.java
@@ -1,7 +1,7 @@
-
+import java.io.IOException;
 public class tester
 {
-  public static void main(String[] args){
+  public static void main(String[] args)throws IOException{
     new hw3().hw3(args[0],args[1]);
   }
 }
