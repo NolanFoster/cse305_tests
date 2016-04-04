a158ac533f6637655c4c5d2ed3d12149df2f4592
Nolan Foster <nolanfoster@gmail.com>
2016-04-04 09:38:14 -0400

    Added java support

--- hw3_test.py
+++ hw3_test.py
@@ -1,25 +1,46 @@
 #!/usr/bin/python
 #!python
-import hw3 ,filecmp, os, sys
-
+import filecmp, os, sys
 path = os.getenv('HOME') + '/Documents/CSE305/hw3/'
+sys.path.insert(0, path+'/python')
+import hw3
 
 def python_test(hw):
     for f in os.listdir(path+"test_cases/input/"+hw):
         if os.path.isfile(os.path.join(path+"test_cases/input/"+hw, f)):
             try:
-                hw3.hw3(path+"test_cases/input/"+hw+"/"+f,f.replace('input', 'output'))
+                hw3.hw3(path+"test_cases/input/"+hw+"/"+f,'python_'+f.replace('input', 'output'))
             except:
                 print ("Unexpected error:", sys.exc_info())
 
-        if(filecmp.cmp(path+"test_cases/output/"+f.replace('input', 'output'), f.replace('input', 'output'))):
+        if(filecmp.cmp(path+"test_cases/output/"+f.replace('input', 'output'), 'python_'+f.replace('input', 'output'))):
             print("            "+f+" : \033[1;32mPassed\033[0;0m")
-            os.remove(f.replace('input', 'output'))
+            os.remove('python_'+f.replace('input', 'output'))
         else:
             print("            "+f+" : \033[1;31mFailed\033[0;0m")
 
-print("HW 2 Tests:")
+def java_test(hw):
+    os.system('javac tester.java')
+    for f in os.listdir(path+"test_cases/input/"+hw):
+        if os.path.isfile(os.path.join(path+"test_cases/input/"+hw, f)):
+            try:
+                os.system('java tester ' + path+"test_cases/input/"+hw+"/"+f+' '+'java_'+f.replace('input', 'output'))
+            except:
+                print ("Unexpected error:", sys.exc_info())
+
+        if(filecmp.cmp(path+"test_cases/output/"+f.replace('input', 'output'), 'java_'+f.replace('input', 'output'))):
+            print("            "+f+" : \033[1;32mPassed\033[0;0m")
+            os.remove('java_'+f.replace('input', 'output'))
+        else:
+            print("            "+f+" : \033[1;31mFailed\033[0;0m")
+
+    os.remove('tester.class')
+    os.remove('hw3.class')
+
+print("HW 2 Python Tests:")
 python_test("hw2")
+print("HW 2 Java Tests:")
+java_test("hw2")
 
 print("HW 3 Tests:")
 python_test("hw3")
---
+++ tester.java
@@ -0,0 +1,7 @@
+
+public class tester
+{
+  public static void main(String[] args){
+    new hw3().hw3(args[0],args[1]);
+  }
+}
